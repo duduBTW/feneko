aaa2f254719245d98cf7741e2d18cdc7323f7504
Carlos Eduardo Alves do Nascimento <carloseduardo108090@hotmail.com>
2021-06-26 23:02:04 -0300

    with portal

---
+++ src/models/itemPedido.ts
@@ -0,0 +1,52 @@
+import { Document, Model, model, Schema } from "mongoose";
+import { IFenekoTag } from "./tags";
+
+/**
+ * Interface to model the Feneko Tag for TypeScript.
+ * @param rag:ref => IFenekoTag._id
+ * @param titlePt:string
+ * @param titleEn:string
+ * @param descPt:string
+ * @param descEn:string
+ * @param image:string
+ */
+export interface IFenekoTipoPedido extends Document {
+  types: IFenekoTag["_id"];
+  name: string;
+  titleEn: string;
+  titlePt: string;
+  descPt: string;
+  descEn: string;
+  image: string;
+}
+
+const fenekoTipoPedidoSchema: Schema = new Schema({
+  types: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "FenekoTipoPedido",
+    },
+  ],
+  titlePt: {
+    type: String,
+    required: true,
+  },
+  titleEn: {
+    type: String,
+    required: true,
+  },
+  descPt: {
+    type: String,
+    required: true,
+  },
+  descEn: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+export default fenekoTipoPedidoSchema;
--- src/pedido/body/design.tsx
+++ src/pedido/body/design.tsx
@@ -6,9 +6,9 @@ export default function Design() {
     <div>
       {" "}
       <ItemPedido
+        type="design"
         types={["emotes", "overlay", "alerta"]}
         title="Design"
-        type="design"
         desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptate provident, tempora aspernatur exercitationem quisquam. Neque sit eaque dignissimos repellendus."
         image="https://pbs.twimg.com/media/E0oFnokUUAUH-hM?format=jpg&name=large"
       />
